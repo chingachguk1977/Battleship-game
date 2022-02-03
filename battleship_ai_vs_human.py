50384451bc25fd254a9fd24c4052f25eb88ef7c6
Roman Bykov <97618922+chingachguk1977@users.noreply.github.com>
2022-02-03 23:43:49 +0300

    Did something in PyCharm

--- battleship_ai_vs_human.py
+++ battleship_ai_vs_human.py
@@ -1,12 +1,15 @@
 import random
 
+
 class GameBoard:
     """
     Game Board class that receives either AI board or user's board at the init
     """
+
     def __init__(self, board):
         self.board = board
 
+    @staticmethod
     def get_letters2numbers():
         """
         :return: the dict for translation of column names into numbers, 
@@ -28,14 +31,15 @@ class GameBoard:
         for row in self.board:
             print(f"{row_num}|{'|'.join(row)}|")
             row_num += 1
-            
-            
+
+
 class Battleship:
     """1 корабль на 3 клетки, 2 корабля на 2 клетки, 4 корабля на одну клетку
     """
+
     def __init__(self, board) -> None:
         self.board = board
-        
+
     def create_ships(self):
         """self here refers to the AI board
 
@@ -48,24 +52,24 @@ class Battleship:
                 self.x_row, self.y_column = random.randint(0, 7), random.randint(0, 7)  # if yes, re-randomize
             self.board[self.x_row][self.y_column] = 'X'
         return self.board
-    
+
     def get_user_input(self):
         try:
             x_row = input("Enter the row of the ship: ")
             while x_row not in '12345678':  # тут надо обработку исключений поставить будет
                 print("Inappropriate choice, please enter a valid row.")
                 x_row = input("Enter the row of the ship: ")
-            
+
             y_column = input("Enter the column letter of the ship: ").upper()
             while y_column not in 'ABCDEFGH':  # тут надо обработку исключений поставить будет
                 print("Inappropriate choice, please enter a valid column.")
                 y_column = input("Enter the column letter of the ship: ").upper()
             return int(x_row) - 1, GameBoard.get_letters2numbers()[y_column]  # return a tuple with coords
-        
+
         except ValueError and KeyError:  # message and keep calling the same method until the input is valid
             print("Not a valid input")
             return self.get_user_input()
-        
+
     def count_hit_ships(self):
         hit_ships = 0
         for row in self.board:
@@ -73,28 +77,29 @@ class Battleship:
                 if column == 'X':
                     hit_ships += 1
         return hit_ships
-    
+
+
 def RunGame():
-    ai_board = GameBoard([[' ']*8 for _ in range(8)])
-    guess_board = GameBoard([[' ']*8 for _ in range(8)])
-    
+    ai_board = GameBoard([[' '] * 8 for _ in range(8)])
+    guess_board = GameBoard([[' '] * 8 for _ in range(8)])
+
     Battleship.create_ships(ai_board)  # call for placing all the ships and store in ai_board
-    
+
     turns = 10
     while turns > 0:
         GameBoard.print_board(guess_board)
         user_x_row, user_y_column = Battleship.get_user_input(object)  # getting user input where to shoot
         while guess_board.board[user_x_row][user_y_column] == '-' or \
-            guess_board.board[user_x_row][user_y_column] == 'X':
-                print("You've already shot these coordinates.")
-                user_x_row, user_y_column = Battleship.get_user_input(object)  # retry the user input where to shoot
+                guess_board.board[user_x_row][user_y_column] == 'X':
+            print("You've already shot these coordinates.")
+            user_x_row, user_y_column = Battleship.get_user_input(object)  # retry the user input where to shoot
         if ai_board.board[user_x_row][user_y_column] == "X":
             print("You sunk one of my ships!")
             guess_board.board[user_x_row][user_y_column] = "X"  # a ship is hit/sunk and need to mark it on user's board
         else:
             print("You missed!")
             guess_board.board[user_x_row][user_y_column] = "-"  # a miss
-            
+
         if Battleship.count_hit_ships == 5:
             print("All ships were sunk")
             break
@@ -105,7 +110,7 @@ def RunGame():
                 print("You ran out of turns, game over.")
                 GameBoard.print_board(guess_board)
                 break
-                
-                
+
+
 if __name__ == '__main__':
     RunGame()
